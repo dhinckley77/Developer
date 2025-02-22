2838091fd8d75c2645d17f4b975da7f4b908e0eb
dhinckley77 <77864077+dhinckley77@users.noreply.github.com>
2025-02-22 16:35:56 -0700

    uploading first commit

---
+++ QueueReceiver/QueueReceiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Messaging.ServiceBus;
+
+//connection string to your service bus namespace
+
+string connectionString = "Endpoint=sb://danielsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ac5wYBHFYL32xtKaiJkoMwxhKrNZyXA/s+ASbNQcFEo=";
+
+// name of your Service Bus queue
+
+string queueName = "queue1";
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the queue
+ServiceBusProcessor processor;
+
+// Create the client object that will be used to create sender and receiver objects
+client = new ServiceBusClient(connectionString);
+
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
---
+++ ServiceBusSender/ServiceBusSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusSender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://danielsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ac5wYBHFYL32xtKaiJkoMwxhKrNZyXA/s+ASbNQcFEo=";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            await SendMessagesAsync(numberOfMessages);
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue. 
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    // Write the body of the message to the console. 
+                    Console.WriteLine($"Sending message: {messageBody}");
+                    // Send the message to the queue. 
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
